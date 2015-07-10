d17d33978e3b74ae34a9e150a509962c90e28497
mraible <matt@raibledesigns.com>
2015-07-09 21:21:42 -0600

    A better baseline for Java EE Web Security.

--- javaee7-security/src/main/java/security/HealthResource.java
+++ javaee7-security/src/main/java/security/HealthResource.java
@@ -1,17 +1,14 @@
 package security;
 
-import org.springframework.stereotype.Component;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Component
 @Path("/health")
-public class HealthController {
+public class HealthResource {
     @GET
     @Produces("application/json")
     public Health health() {
-        return new Health("Jersey: Up and Running!");
+        return new Health("Jersey is up and running!");
     }
 }
