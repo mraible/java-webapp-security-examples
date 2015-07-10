057bec12bef4a307d5343b854801791543704151
mraible <matt@raibledesigns.com>
2015-07-10 00:20:22 -0600

    Created skeleton Spring Boot application with web and security support.

---
+++ spring-security/src/main/java/security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+}
