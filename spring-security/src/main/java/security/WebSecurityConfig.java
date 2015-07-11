1ae83aeb6975a107dcdb616eeae63bc846fcadaf
Matt Raible <matt@raibledesigns.com>
2015-07-11 14:28:48 -0600

    Enabled csrf by default.

--- spring-security/src/main/java/security/WebSecurityConfig.java
+++ spring-security/src/main/java/security/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf().and()
             .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/login").permitAll()
