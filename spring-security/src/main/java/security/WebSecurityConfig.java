61fe6247c477528899dc79551954550f03d99eec
Rob Winch <rwinch@gopivotal.com>
2015-07-15 11:17:26 -0500

    Remove Unnecessary Clutter from WebSecurityConfig
    - csrf is enabled by default
    - logoutUrl is /logout by default
    - rememberMe support automatically removes the remember-me cookie
    - the logout success url is /login?logout by default
    - cleaned unused imports

--- spring-security/src/main/java/security/WebSecurityConfig.java
+++ spring-security/src/main/java/security/WebSecurityConfig.java
@@ -1,14 +1,11 @@
 package security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -22,7 +19,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().and()
             .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/login").permitAll()
@@ -33,9 +29,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .logoutUrl("/logout")
-                .deleteCookies("remember-me")
-                .logoutSuccessUrl("/login?logout")
                 .permitAll()
                 .and()
             .rememberMe();
@@ -43,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(this.dataSource);
+        auth
+            .jdbcAuthentication()
+                .dataSource(this.dataSource);
     }
 }
